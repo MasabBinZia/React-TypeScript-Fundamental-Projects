20fa87aef184474dff565fe7fd07bfd3104b0aff
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-05-25 21:12:03 +0500

    tailwindcss added-04

--- 04-accordion/src/App.tsx
+++ 04-accordion/src/App.tsx
@@ -6,9 +6,9 @@ function App() {
   const [questions, setQuestions] = useState(data);
 
   return (
-    <main>
-      <div className='container'>
-        <h3>Questions and Answers About Web 3.0 </h3>
+    <main >
+      <div className='container w-auto mt-20 mr-auto ml-auto bg-white rounded pt-10 pr-8 max-w-4xl grid'>
+        <h3 className='ml-6 font-medium font-bold'>Questions and Answers About Web 3.0 </h3>
         <section className='info'>
           {questions.map((question) => {
             return (
--- 04-accordion/src/components/Question.tsx
+++ 04-accordion/src/components/Question.tsx
@@ -10,14 +10,14 @@ const Question = ({ title, info }: QuestionProps) => {
   const [showInfo, setShowInfo] = useState<boolean>(false);
 
   return (
-    <article className='question'>
-      <header>
-        <h4>{title}</h4>
-        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+    <article className='question p-4 border border-gray-300 mb-4 rounded shadow-lg  '>
+      <header className='flex justify-between items-center'>
+        <h4 className='normal-case leading-normal'>{title}</h4>
+        <button className='btn bg-transparent border-transparent w-8 h-8 bg-gray-100 flex items-center justify-center rounded-full text-pink-600 cursor-pointer ml-4 self-center min-w-8' onClick={() => setShowInfo(!showInfo)}>
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {showInfo && <p>{info}</p>}
+      {showInfo && <p className='text-gray-500 mt-2 mb-0'>{info}</p>}
     </article>
   );
 };
