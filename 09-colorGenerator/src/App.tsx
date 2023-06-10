7d1d7c9baf564d7014966ac96e95e259f825f131
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-06-10 21:27:43 +0500

    09-colorGenerator-css

--- 09-colorGenerator/src/App.tsx
+++ 09-colorGenerator/src/App.tsx
@@ -1,14 +1,55 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+import Values from 'values.js';
 
 function App() {
+  const [color, setColor] = useState<string>('');
+  const [error, setError] = useState<boolean>(false);
+  const [list, setList] = useState<Values[]>(new Values('#f15025').all(10));
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <section className='container text-center flex items-center h-20 pl-8'>
+        <h3 className='mb-0 mr-8'>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : ''} `}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              rgb={color.rgb}
+              weight={color.weight}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
